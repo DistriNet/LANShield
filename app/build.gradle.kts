a73d20e0730e48f6ac55f0c91bbe00f6a12e5c4a
Jeroen Robben <jeroen@robben.io>
2025-04-14 17:03:21 +0200

    Release v0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "org.distrinet.lanshield"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8"
+        versionCode = 9
+        versionName = "0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
