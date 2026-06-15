7fc89820f0e29c8fee67f024f4410797f7c8cf73
Jeroen Robben <jeroen@robben.io>
2026-06-15 14:29:31 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
 
 //    id("com.google.gms.go[DELETEME]ogle-services") version "4.4.4" apply false
-//    id("com.google.fire[DELETEME]base.crashlytics") version "3.0.6" apply false
+//    id("com.google.fire[DELETEME]base.crashlytics") version "3.0.7" apply false
 }
